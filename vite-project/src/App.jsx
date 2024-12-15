8ec78cb5475f7917b9ca740907b6dddffdf966e7
Saba Lomsadze <103242407+Lomsa005@users.noreply.github.com>
2024-12-15 19:13:19 +0000

    firs

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,16 @@
+import Scene from "./Model"
+
+function App() {
+
+  return (
+    <>
+      <div style={{ width: '100vw', height: '100vh' }}>
+       
+       <Scene/>
+     
+   </div>
+    </>
+  )
+}
+
+export default App
